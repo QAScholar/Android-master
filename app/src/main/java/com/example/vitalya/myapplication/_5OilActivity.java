08bf5b31328418d552ded7f7925872bfcc694254
QAScholar <qa.scholar@yandex.ru>
2018-01-25 15:33:39 +1000

    25.01

--- app/src/main/java/com/example/vitalya/myapplication/_5OilActivity.java
+++ app/src/main/java/com/example/vitalya/myapplication/_5OilActivity.java
@@ -1,26 +1,42 @@
 package com.example.vitalya.myapplication;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
-public class _5OilActivity extends AppCompatActivity {
+public class _5OilActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button MainMenu;
-    private Spinner spinner1, spinner2;
+    //private Spinner spinner1, spinner2;
+    //Создаем переменные для вызова календаря
+    private EditText callcalendar;
+    private DatePickerDialog datepicker;
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout._5activity_oil);
+        //Указываем формат даты
+        dateFormatter=new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        //Создаем методы для запуска календаря
+        findViewsByID();
+        setDateTimeField();
 
         MainMenu = findViewById(R.id.buttonMainMenu);
         MainMenu.setOnClickListener(new View.OnClickListener() {
@@ -30,48 +46,33 @@ public class _5OilActivity extends AppCompatActivity {
                 startActivity(GoToMainMenu);
             }
         });
-//        addItemsOnSpinner2();
-//    }
-//        //addListenerOnButton();
-//       // addListenerOnSpinnerItemSelection();
-//    public void addItemsOnSpinner2() {
 //
-//        spinner2 = (Spinner) findViewById(R.id.spinner2);
-//        List<String> list = new ArrayList<String>();
-//        list.add("list 1");
-//        list.add("list 2");
-//        list.add("list 3");
-//        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-//                android.R.layout.simple_spinner_item, list);
-//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner2.setAdapter(dataAdapter);
-//    }
-
-//    public void addListenerOnSpinnerItemSelection() {
-//        spinner1 = (Spinner) findViewById(R.id.spinner1);
-//        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-//    }
-
-    // get the selected dropdown list value
-//    public void addListenerOnButton() {
-//
-//        spinner1 = (Spinner) findViewById(R.id.spinner1);
-//        spinner2 = (Spinner) findViewById(R.id.spinner2);
-//        btnSubmit = (Button) findViewById(R.id.btnSubmit);
-//
-//        btnSubmit.setOnClickListener(new OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//
-//                Toast.makeText(_5OilActivity.this,
-//                        "OnClickListener : " +
-//                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
-//                                "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//
-//        });
     }
+    private void findViewsByID() {
+        callcalendar=(EditText) findViewById(R.id.editTextCalendar);
+        callcalendar.setInputType(InputType.TYPE_NULL);
+        callcalendar.requestFocus();
+    }
+    private void setDateTimeField() {
+        callcalendar.setOnClickListener(this);
+        Calendar newCalendar=Calendar.getInstance();
+        datepicker=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate=Calendar.getInstance();
+                newDate.set(year,month,dayOfMonth);
+                callcalendar.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == callcalendar) {
+            datepicker.show();
+        }
+    }
+
+    
 }
 
